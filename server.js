27fd7788868090abb4d235cf1d4cc830c467cc80
whitersun <37898596+whitersun@users.noreply.github.com>
2020-06-01 19:32:19 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+const serverStatic = require('serve-static')
+
+const app = express()
+app.use('/', serverStatic(path.join(__dirname, '/dist')))
+
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'))
+})
+
+const port = process.env.PORT || 5000
+app.listen(port)
+
+console.log(`Port ready on port: ${port}`)
